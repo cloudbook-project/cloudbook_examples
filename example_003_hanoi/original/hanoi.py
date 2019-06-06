760e070d553801069f29261a8249f9cecd412000
Garcia Aranda <jose_javier.garcia_aranda@nokia.com>
2019-06-06 15:01:41 +0200

    hanoi

--- example_003_hanoi/original/hanoi.py
+++ example_003_hanoi/original/hanoi.py
@@ -1,14 +1,16 @@
 import sys
 
 #__CLOUDBOOK:RECURSIVE__
-def move_tower(height,origin, destination,intermediate):
+def move_tower(height,origin, destination,intermediate,level):
+	level=level+1
+	print ("=====================================> DEPTH LEVEL:",level)
 	if height>=1:
-		move_tower(height-1,origin,intermediate,destination)
+		move_tower(height-1,origin,intermediate,destination,level)
 		print ("move disc from "+str(origin)+" to "+str(destination))
-		move_tower(height-1,intermediate,destination,origin)
+		move_tower(height-1,intermediate,destination,origin,level)
 
 def main():
 	#sys.setrecursionlimit(10)#El tope de hanoi es 5 pisos con esto
-	move_tower(5,1,2,3)
+	move_tower(1500,1,2,3,0)
 
 main()
